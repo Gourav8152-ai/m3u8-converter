d501d9fc5f7bf10252bbc9d51c1884d63f5d033a
Gorav Agarwal <55653105+Gourav8152-ai@users.noreply.github.com>
2023-06-14 11:09:32 +0530

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -5,18 +5,28 @@ const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 
+function isValidM3U8URL(url) {
+  const urlRegex = /^https?:\/\/[^\s/$.?#].[^\s]*$/;
+  const extension = url.split('.').pop().toLowerCase();
+  return urlRegex.test(url) && extension === 'm3u8';
+}
+
 function createBot() {
   const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
   bot.start((ctx) => ctx.reply('Welcome to the M3U8 to MP3 converter bot! Send me an M3U8 file or URL to convert.'));
+
   bot.on('message', async (ctx) => {
     if (ctx.message.document && ctx.message.document.mime_type === 'application/x-mpegURL') {
-      const file = await bot.telegram.getFile(ctx.message.document.file_id);
-      const inputFile = `https://api.telegram.org/file/bot${bot.token}/${file.file_path}`;
-      convertM3U8(inputFile, ctx);
+      const inputFile = `https://api.telegram.org/file/bot${bot.token}/${ctx.message.document.file_path}`;
+      await convertM3U8(inputFile, ctx);
     } else if (ctx.message.text && ctx.message.text.endsWith('.m3u8')) {
       const inputURL = ctx.message.text;
-      convertM3U8(inputURL, ctx);
+      if (isValidM3U8URL(inputURL)) {
+        await convertM3U8(inputURL, ctx);
+      } else {
+        ctx.reply('Please provide a valid M3U8 URL.');
+      }
     } else {
       ctx.reply('Please send a valid M3U8 file or URL.');
     }
@@ -25,19 +35,23 @@ function createBot() {
   function convertM3U8(input, ctx) {
     const outputFilename = 'output.mp3'; // Default output filename
     const output = path.join(__dirname, outputFilename);
-    
-    const command = ffmpeg()
-      .setFfmpegPath(ffmpegInstaller.path)
-      .input(input)
-      .output(output)
-      .on('end', () => {
-        ctx.replyWithAudio({ source: createReadStream(output) });
-      })
-      .on('error', (err) => {
-        console.error(err);
-        ctx.reply('An error occurred during the conversion.');
-      })
-      .run();
+
+    return new Promise((resolve, reject) => {
+      ffmpeg()
+        .setFfmpegPath(ffmpegInstaller.path)
+        .input(input)
+        .output(output)
+        .on('end', () => {
+          ctx.replyWithAudio({ source: createReadStream(output) });
+          resolve();
+        })
+        .on('error', (err) => {
+          console.error(err);
+          ctx.reply('An error occurred during the conversion. Please try again later.');
+          reject(err);
+        })
+        .run();
+    });
   }
 
   return bot;
